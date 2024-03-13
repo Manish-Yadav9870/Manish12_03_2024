9ecef44bcb0cf2b074d3c8217a75f24febac7011
Manish Yadav <myadav9870@gmail.com>
2024-03-13 17:49:29 +0530

    update

---
+++ intro.js
@@ -0,0 +1,66 @@
+// --------Q.1---------
+
+
+// ----primitives (value type)-------
+// ------1.number----------
+let a=10;
+console.log(typeof a)
+// ------2.string---------
+let data="mukesh";
+console.log(typeof data)
+// ------3.boolean-----
+let mohit=true;
+let manish=false;
+console.log(typeof mohit)
+// -------4.undefined-------
+let user;
+console.log(typeof user)
+// -----------5.null---------
+let value=null;
+value=20;
+if(value==25){
+    user=100;
+}
+else{
+    user=30;
+}
+console.log(user)
+// ----Non primitives (value type)-------
+// -----1.object---------
+let person={
+    firstname:"mukesh",
+    lastname:"jat",
+    age:30
+}
+console.log(typeof person)
+console.log(person.firstname)
+console.log(person.lastname)
+console.log(person.age)
+// ------2.array--------
+let c=["mukesh","mohit",1,2,3]
+console.log(typeof c)
+// -----3.function----------
+function mk (a){
+    console.log(a)
+}
+
+
+// ----------Q.2------------
+//java script is a single theraded programming (scripting) language
+
+//----------Q.3------------
+//An array is a collection of items of same data type stored at contiguous memory locations
+
+//---------Q.4------------
+//objects are basic concepts of Object Oriented Programming (OOPs) that are used to represent real-world concepts and entities. 
+
+//------Q.5-------------
+//        Primitive data structure                                    	     Non-primitive data structure
+//Primitive data structure is a kind of data structure                Non-primitive data structure is a type of data structure
+//      that stores the data ofonly one type.                               that can store the data of more than one type.
+
+//--------------Q.6--------------
+//key words for declare variables in js is 
+//1.let (block scope)
+//2.var (global scope)
+//3.const (block scope)
